eba03de335cd4fa8a3625a31acc6a6841d34c7b0
Koen Corneillie <koen.corneillie@MacBook-Pro.local>
2020-10-22 12:20:03 +0200

    comment CRUD

--- util/checkAuth.js
+++ util/checkAuth.js
@@ -13,7 +13,7 @@ module.exports = (context) => {
         const user = jwt.verify(token, SECRET_KEY);
         return user;
       } catch (err) {
-        throw new AuthenticationError("Invalic/Expired token");
+        throw new AuthenticationError("Invalid/Expired token");
       }
     }
     throw new Error("Authentication token must be 'Bearer [token]'");
