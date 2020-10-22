f0d89e8c8704463f6c7be81d1fccd6b1ddce6c93
Koen Corneillie <koen.corneillie@MacBook-Pro.local>
2020-10-22 16:15:45 +0200

    comments & likes

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 
@@ -31,7 +31,12 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user);
+
+      // check if post is not empty
+      if (body.trim() === "") {
+        throw new Error("Post must not be empty");
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
@@ -40,7 +45,9 @@ module.exports = {
       });
 
       const post = await newPost.save();
-
+      context.pubsub.publish("NEW_POST", {
+        newPost: post,
+      });
       return post;
     },
 
@@ -58,6 +65,33 @@ module.exports = {
       }
 
       return post;
+    },
+
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already liked, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username: username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST"),
     },
   },
 };
