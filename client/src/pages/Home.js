9686a460ffde551551df736831b649166500509c
Koen Corneillie <koen.corneillie@be10820.ad.tbwagroup.be>
2020-10-29 12:18:58 +0100

    display posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+
+import { Grid } from "semantic-ui-react";
+
+import PostCard from "../components/PostCard";
 
 function Home() {
-  return <div>BLABLABLABLA</div>;
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+  let posts;
+
+  if (data) {
+    posts = data.getPosts;
+  }
+
+  return (
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent posts</h1>
+      </Grid.Row>
+
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          posts &&
+          posts.map((post) => {
+            return (
+              <Grid.Column key={post.id} style={{ marginBottom: "20px" }}>
+                <PostCard post={post} />
+              </Grid.Column>
+            );
+          })
+        )}
+      </Grid.Row>
+    </Grid>
+  );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
